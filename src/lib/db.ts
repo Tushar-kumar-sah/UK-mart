a4aeffe0b647ffcef13ad194b92cd9689ca633f3
Tushar-kumar-sah <sah05tushar@gmail.com>
2026-06-19 17:27:44 +0530

    db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,24 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
 
 const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL!,
-})
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+  url: process.env.DATABASE_URL,
+});
 
 export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
-    log: ['query'],
-  })
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
