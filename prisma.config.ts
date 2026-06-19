79b1b491627ceb60527eb13bf382ed332a90e500
Tushar-kumar-sah <sah05tushar@gmail.com>
2026-06-19 19:54:35 +0530

    mm

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: "postgresql://neondb_owner:npg_9L3btpSmMsCQ@ep-mute-leaf-atsnq5pg-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
